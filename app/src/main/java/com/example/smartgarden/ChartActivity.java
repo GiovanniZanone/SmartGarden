0cbbebd963c706fbacd81368ce95c0f03c3574c2
gioza <giozanone@gmail.com>
2022-08-11 04:01:40 +0200

    graph chart  WIP

--- app/src/main/java/com/example/smartgarden/ChartActivity.java
+++ app/src/main/java/com/example/smartgarden/ChartActivity.java
@@ -4,7 +4,10 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,21 +20,25 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LabelFormatter;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DateFormat;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ChartActivity extends AppCompatActivity {
+public class ChartActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     DatabaseReference mPostReference;
     private GraphView graph;
@@ -41,6 +48,9 @@ public class ChartActivity extends AppCompatActivity {
     private LineGraphSeries<DataPoint> series_temperature;
     private LineGraphSeries<DataPoint> series_light;
     private LineGraphSeries<DataPoint> series_pressure;
+    private RadioButton elementSelected;
+    private Calendar mCalendar;
+    private DateFormat mDateFormat = new SimpleDateFormat("HH:mm:ss:SS");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +60,23 @@ public class ChartActivity extends AppCompatActivity {
         mPostReference = FirebaseDatabase.getInstance().getReference("SmartGarden");
 
         setSupportActionBar(myToolbar);
-
+        Spinner spinner = (Spinner) findViewById(R.id.simpleSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.period_length_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner and add listener
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
-
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        //select moisture
+        elementSelected=findViewById(R.id.radioButton);
+        elementSelected.setChecked(true);
 
         graph = findViewById(R.id.Graph);
         Viewport vp = graph.getViewport();
@@ -63,7 +84,6 @@ public class ChartActivity extends AppCompatActivity {
         vp.setMaxX(200);
         vp.setMinX(0);
 
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
         graph.getGridLabelRenderer().setHorizontalLabelsAngle(135);
         series_moisture = new LineGraphSeries<>(new DataPoint[]{
                 new DataPoint(0,0)
@@ -86,41 +106,44 @@ public class ChartActivity extends AppCompatActivity {
         });
 
         DatabaseReference databaseReference = mPostReference.child("SensorsData");
-        ChildEventListener childEventListener = new ChildEventListener() {
+        Query query = databaseReference.limitToFirst(60);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                Log.d("Datasnapshot: ", "String" + snapshot.getValue());
-                HashMap<String, String> result = (HashMap<String, String>) snapshot.getValue();
-                series_moisture.appendData(new DataPoint(i, Double.parseDouble(result.get("moisture"))),false, 1000, false);
-                series_humidity.appendData(new DataPoint(i, Double.parseDouble(result.get("humidity"))),false, 1000, false);
-                series_temperature.appendData(new DataPoint(i, Double.parseDouble(result.get("temp"))),false, 1000, false);
-                series_light.appendData(new DataPoint(i, Double.parseDouble(result.get("light"))),false, 1000, false);
-                series_pressure.appendData(new DataPoint(i, Double.parseDouble(result.get("pressure"))),false, 1000, false);
-                graph.invalidate();
-                graph.getViewport().scrollToEnd();
-                i++;
-            }
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot element : snapshot.getChildren()) {
+                    HashMap<String, String> result = (HashMap<String, String>) element.getValue();
+                    series_moisture.appendData(new DataPoint(Double.parseDouble(element.getKey()), Double.parseDouble(result.get("moisture"))),false, 60, false);
+                    series_humidity.appendData(new DataPoint(Double.parseDouble(element.getKey()), Double.parseDouble(result.get("humidity"))),false, 60, false);
+                    series_temperature.appendData(new DataPoint(Double.parseDouble(element.getKey()), Double.parseDouble(result.get("temp"))),false, 60, false);
+                    series_light.appendData(new DataPoint(Double.parseDouble(element.getKey()), Double.parseDouble(result.get("light"))),false, 60, false);
+                    series_pressure.appendData(new DataPoint(Double.parseDouble(element.getKey()), Double.parseDouble(result.get("pressure"))),false, 60, false);
+                    graph.invalidate();
+                    graph.getViewport().scrollToEnd();
+                    i++;
+                }
+                graph.removeAllSeries();
+                graph.addSeries(series_moisture);
+                Log.d("series", String.valueOf(series_moisture));
+                graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                    @Override
+                    public String formatLabel(double value, boolean isValueX) {
+                        if (isValueX) {
+                            mCalendar.setTimeInMillis((long) value);
+                            System.out.println(mDateFormat.format(mCalendar.getTimeInMillis()));
+                            return mDateFormat.format(mCalendar.getTimeInMillis());
+                        } else {
+                            return super.formatLabel(value, isValueX);
+                        }
+                    }
+                });
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        };
-        databaseReference.addChildEventListener(childEventListener);
+        });
+
     }
 
     public void checkChart(View view){
@@ -157,25 +180,62 @@ public class ChartActivity extends AppCompatActivity {
     public void moisture_chart(){
         graph.removeAllSeries();
         graph.addSeries(series_moisture);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+
     }
 
     private void humidity_chart() {
         graph.removeAllSeries();
         graph.addSeries(series_humidity);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+
     }
 
     private void temperature_chart() {
         graph.removeAllSeries();
         graph.addSeries(series_temperature);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+
     }
 
     private void light_chart() {
         graph.removeAllSeries();
         graph.addSeries(series_light);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+
     }
 
     private void pressure_chart() {
         graph.removeAllSeries();
         graph.addSeries(series_pressure);
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Object selectedItem = adapterView.getSelectedItem();
+        switch (selectedItem.toString()){
+            case "last hour":
+                Log.d("ITEM SELECTED", selectedItem.toString());
+                break;
+            case "last six hours":
+                Log.d("ITEM SELECTED", selectedItem.toString());
+                break;
+            case "last day":
+                Log.d("ITEM SELECTED", selectedItem.toString());
+                break;
+            case "last week":
+                Log.d("ITEM SELECTED", selectedItem.toString());
+                break;
+            case "last month":
+                Log.d("ITEM SELECTED", selectedItem.toString());
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
